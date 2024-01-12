b91ea3f9d20f00d0fdcf3b8b367432c5283ec887
Ganga Mahesh Siddem <gangams@microsoft.com>
2024-01-11 23:17:55 -0800

    minimal changes for fips repro

--- main.sh
+++ main.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-# Get the start time of the setup in seconds
-startTime=$(date +%s)
-endTime=$(date +%s)
-elapsed=$((endTime-startTime))
-echo "startup script took: $elapsed seconds"
-
-cat packages_version.txt
-
-
-sleep inf &
-wait
+while true; do
+    echo "Container is running..."
+    sleep 10
+done
